4ebcd87ba4f7ed924e793f123ba91154311a5886
kimtaeho <xxxxoghxxx@naver.com>
2019-09-19 17:42:05 +0900

    Updates

---
+++ precache-manifest.b008da18588fbf70e44afe7f472fc4d9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e315fb7fad2a55c15b1377488ea010eb",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "bdd8a808aa278518fafc",
+    "url": "/movie_app_2019/static/js/2.86a49d24.chunk.js"
+  },
+  {
+    "revision": "fd5106c8ee2fa38bf1a0",
+    "url": "/movie_app_2019/static/js/main.531b7f93.chunk.js"
+  },
+  {
+    "revision": "29aaad419714eb018816",
+    "url": "/movie_app_2019/static/js/runtime~main.f4904fd8.js"
+  }
+]);
